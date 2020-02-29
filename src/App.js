37e0aab48385185df885bfcf7a48ffb0b1f3920a
thaveesit douangaphaivong <d15.landon@gmail.com>
2020-02-28 19:44:00 -0600

    very basic styling

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import Signup from './components/user_components/Signup';
 function App() {
   return (
     <div className="App">
-      <h1>QUAKE FO SHO staging</h1>
-      <Login/>
+      <h1>Quake Live</h1>
       <Signup/>
     </div>
   );
