ec25b4a5c429cf3d6137328555151873509790ed
Julian Yates <julianyates@Julians-MacBook-Pro.local>
2020-03-04 21:00:28 -0600

    added unstyled navigation bar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,1 +1,17 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
 
+
+function Navigation(props) {
+    return (
+        <div className="navigation">
+            <Link to="/home">Home</Link>
+            <Link to="/dashboard">Dashboard</Link>
+            <Link to="/safetytips">Safety tips</Link>
+            <Link to="/signup">Sign up</Link>
+            <Link to="/login">Log in</Link>
+            <Link to="/logout">Logout</Link>
+        </div>
+    );
+}
+export default Navigation;
