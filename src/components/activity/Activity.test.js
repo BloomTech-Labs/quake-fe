3a7b19d3c350a4e2ae5f44aff6a60522d21060cd
Ken Diehl <diehlkj.99@gmail.com>
2020-05-30 00:06:18 -0400

    PR Contributors include Eddie Jimenez and Jeff Breig

--- src/components/activity/Activity.test.js
+++ src/components/activity/Activity.test.js
@@ -1,8 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import * as rtl from "@testing-library/react";
 
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
