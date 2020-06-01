9224368c9b0c3c71140ae57c832387bb9bfdfb41
unknown <jjbreig@gmail.com>
2020-06-01 00:25:35 -0400

    restyled nav, waiting for icons, styled collapsed cards

--- src/components/activity/Card.js
+++ src/components/activity/Card.js
@@ -1,20 +1,26 @@
 import React from "react";
 
 function Card({ quake, number }) {
+  console.log(quake.properties);
   return (
     <div className="activity-card">
-      <h1>
-        {number} - {quake.properties.title}
-      </h1>
-      <h2>
-        Location: {quake.geometry.coordinates[0]} /{" "}
-        {quake.geometry.coordinates[1]}
-      </h2>
-      <p>Magnitude: {quake.properties.mag}</p>
-      <p>Depth: {quake.geometry.coordinates[2]} km.</p>
-      <p>Status: {quake.properties.status}</p>
+      <div className="magnitude">{Number.parseFloat(quake.properties.mag).toPrecision(2)}</div>
+      <div className="card-info">
+        <div className="place">{quake.properties.place}</div>
+      </div>
+      <div className="dropdown-arrow"></div>
     </div>
   );
 }
 
 export default Card;
+{/* <h1>
+  {number} - {quake.properties.title}
+</h1>
+  <h2>
+    Location: {quake.geometry.coordinates[0]} /{" "}
+    {quake.geometry.coordinates[1]}
+  </h2>
+  <p>Magnitude: {quake.properties.mag}</p>
+  <p>Depth: {quake.geometry.coordinates[2]} km.</p>
+  <p>Status: {quake.properties.status}</p> */}
