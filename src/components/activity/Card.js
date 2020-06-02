a8e56a56df04acad6c231fa8471c5f13c425c854
unknown <jjbreig@gmail.com>
2020-06-01 22:01:15 -0400

    added some earthquake details

--- src/components/activity/Card.js
+++ src/components/activity/Card.js
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 
 function Card({ quake, number }) {
   const [open, setOpen] = useState(false);
+  console.log(quake);
   return (
     <div>
       <div className="activity-card" onClick={() => setOpen(open => !open)}>
@@ -12,24 +13,13 @@ function Card({ quake, number }) {
         <div className={!open ? "dropdown-arrow" :"dropdown-arrow-clicked"} onClick={()=> setOpen(open=>!open)}></div>
       </div>
       <div className={!open ? "activity-details-closed" : "activity-details-open"}>
+        <div className="detail-item">Date & Time: {quake.properties.mag}</div>
+        <div className="detail-item">Location: {quake.geometry.coordinates[0]},{quake.geometry.coordinates[1]}</div>
+        <div className="detail-item">Depth: {quake.geometry.coordinates[2]} km.</div>
         <div className="detail-item">Magnitude: {quake.properties.mag}</div>
-        <div className="detail-item">Depth: {quake.geometry.coordinates[2]} km.</div>
-        <div className="detail-item">Status: {quake.properties.status}</div>
-        <div className="detail-item">Depth: {quake.geometry.coordinates[2]} km.</div>
-        <div className="detail-item">Status: {quake.properties.status}</div>
       </div>
     </div>
   );
 }
 
 export default Card;
-{/* <h1>
-  {number} - {quake.properties.title}
-</h1>
-  <h2>
-    Location: {quake.geometry.coordinates[0]} /{" "}
-    {quake.geometry.coordinates[1]}
-  </h2>
-  <p>Magnitude: {quake.properties.mag}</p>
-  <p>Depth: {quake.geometry.coordinates[2]} km.</p>
-  <p>Status: {quake.properties.status}</p> */}
