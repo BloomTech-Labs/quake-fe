9304c38b0eaa1f45423fbf839b47bb86f4874875
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2020-06-02 20:32:13 -0700

    Adjusted coverage to include international search

--- src/components/activity/Autofill.js
+++ src/components/activity/Autofill.js
@@ -1,41 +1,41 @@
-import React, {Component} from 'react'
-import Geocoder from 'react-mapbox-gl-geocoder'
-
+import React, { Component } from "react";
+import Geocoder from "react-mapbox-gl-geocoder";
 
 const mapAccess = {
-    mapboxApiAccessToken: 'pk.eyJ1IjoiZWRkaWVqZGV2IiwiYSI6ImNrYWoxNmllODA3c3Iyd2xlY25qMWdqZTcifQ.qwSgQ-5q7jNjy2XWojuwwg'
-}
+  mapboxApiAccessToken:
+    "pk.eyJ1IjoiZWRkaWVqZGV2IiwiYSI6ImNrYWoxNmllODA3c3Iyd2xlY25qMWdqZTcifQ.qwSgQ-5q7jNjy2XWojuwwg",
+};
 
 const queryTypes = {
-    types: 'country,region,place,postcode,locality,neighborhood'
-}
-
-const queryParams = {
-    country: 'us'
-}
+  types: "country,region,place,postcode,locality,neighborhood",
+};
 
 class SearchBar extends Component {
-    state = {
-        viewport: {}
-    }
+  state = {
+    viewport: {},
+  };
 
-    onSelected = (viewport, item) => {
-        this.setState({viewport});
-        console.log('Selected: ', item)
-    }
+  onSelected = (viewport, item) => {
+    this.setState({ viewport });
+    console.log("Selected: ", item);
+  };
 
-    render() {
-        const {viewport} = this.state
+  render() {
+    const { viewport } = this.state;
 
-        return (
-            <div>
-                <Geocoder
-                    {...mapAccess} onSelected={this.onSelected} viewport={viewport} hideOnSelect={true}
-                    queryParams={queryParams} types={queryTypes} autocomplete={true}
-                />
-            </div>
-        )
-    }
+    return (
+      <div>
+        <Geocoder
+          {...mapAccess}
+          onSelected={this.onSelected}
+          viewport={viewport}
+          hideOnSelect={true}
+          types={queryTypes}
+          autocomplete={true}
+        />
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
