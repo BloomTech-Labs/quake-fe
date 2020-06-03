035d6c1d52f9d439002bb7a3b213e2769c7bcf51
unknown <jjbreig@gmail.com>
2020-06-02 23:24:54 -0400

    restyled app per UX

--- src/components/activity/Card.js
+++ src/components/activity/Card.js
@@ -3,39 +3,51 @@ var moment = require('moment');
 
 function Card({ quake, number }) {
   const [open, setOpen] = useState(false);
-  console.log(quake);
 
+  // Converting epoch time to a more familiar MM-DD-YYYY / hh:mm format.
   const epoch = new Date(quake.properties.time);
-  var localTime = moment(quake.properties.time).format('MM-DD-YYYY / hh:mm:ss A')
+  var localTime = moment(quake.properties.time).format('MM-DD-YYYY / hh:mm:ss A');
+  
+  // Splitting the quake title into 'distance from', 'city', 'state/nation'
+  const split1 = quake.properties.place.split(', ');
+  const split2 = split1[0].split(' of ');
+  split2.push(split1[1]);
+  split2.push(split2.shift());
 
-  function getDates(theDate) {
-    var month = "" + (theDate.getMonth() + 1),
-      day = "" + theDate.getDate(),
-      year = theDate.getFullYear();
+  //Rounding magnitude to the nearest tenth.
+  const magRound = Math.round(quake.properties.mag * 10) / 10;
 
-    if (month.length < 2) month = "0" + month;
-    if (day.length < 2) day = "0" + day;
-
-    const fullDate = [month, day, year].join("-");
-
-    return fullDate;
-  }
-
-  const magRound = Math.round(quake.properties.mag * 10) / 10
   return (
     <div>
-      <div className="activity-card" onClick={() => setOpen(open => !open)}>
-        <div className="magnitude">{Math.round(quake.properties.mag * 10) / 10}</div>
-        <div className="card-info">
-          <div className="place">{quake.properties.place}</div>
+      <div className="activity-card" onClick={() => setOpen((open) => !open)}>
+        <div className="magnitude">
+          {Math.round(quake.properties.mag * 10) / 10}
         </div>
-        <div className={!open ? "dropdown-arrow" :"dropdown-arrow-clicked"} onClick={()=> setOpen(open=>!open)}></div>
+        <div className="card-info">
+          <div className="place-info">
+            <h2 className="city">{split2[0]}</h2>
+            <h2 className="country">{split2[1]}</h2>
+            <h3 className="distance">{split2[2]}</h3>
+          </div>
+        </div>
+        <div
+          className={!open ? "dropdown-arrow" : "dropdown-arrow-clicked"}
+          onClick={() => setOpen((open) => !open)}
+        ></div>
       </div>
-      <div className={!open ? "activity-details-closed" : "activity-details-open"}>
-        <div className="detail-item">Date &amp; Time: {localTime}</div>
-        <div className="detail-item">Location: {quake.geometry.coordinates[0]},{quake.geometry.coordinates[1]}</div>
-        <div className="detail-item">Depth: {quake.geometry.coordinates[2]} km.</div>
-        <div className="detail-item">Magnitude: {quake.properties.mag}</div>
+      <div
+        className={!open ? "activity-details-closed" : "activity-details-open"}
+      >
+        <div className="detail-item">
+          <strong>Date &amp; Time:</strong> {localTime}
+        </div>
+        <div className="detail-item">
+          <strong>Location:</strong> {quake.geometry.coordinates[0]}, {quake.geometry.coordinates[1]}
+        </div>
+        <div className="detail-item">
+          <strong>Depth:</strong> {quake.geometry.coordinates[2]} km.
+        </div>
+        <div className="detail-item"><strong>Magnitude:</strong> {quake.properties.mag}</div>
       </div>
     </div>
   );
