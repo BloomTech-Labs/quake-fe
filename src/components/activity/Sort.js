4d42dbb776e50f0f9ea883895de2f1ef0ebd4956
Jeff Breig <jjbreig@gmail.com>
2020-06-05 01:16:12 -0400

    started sort component

---
+++ src/components/activity/Sort.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import { quakeFetch } from "../../actions";
+import { useForm } from "../../customHooks/useForm";
+
+function Sort(props) {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
