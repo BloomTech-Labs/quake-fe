ec6728c0b009c408bf998c047a323f31edcd2059
cesarhj19 <cesarhj19@gmail.com>
2020-06-08 11:29:01 -0700

    dark mode feature added, temp switch in the search menu until permanent location found

---
+++ src/customHooks/useDarkMode.js
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = key => {
+  const [darkMode, setDarkMode] = useLocalStorage(key);
+
+  useEffect(() => {
+    if (darkMode) document.body.classList.add('dark-mode');
+    else document.body.classList.remove('dark-mode');
+  });
+  return [darkMode, setDarkMode];
+};
+
+export default useDarkMode;
