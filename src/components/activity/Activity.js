718d4f4cf70585af31f8e384485b9e7600589aed
Jeff Breig <jjbreig@gmail.com>
2020-06-07 22:31:55 -0400

    pulling new master

--- src/components/activity/Activity.js
+++ src/components/activity/Activity.js
@@ -2,11 +2,13 @@ import React from "react";
 import Filters from "./Filters";
 import List from "./List";
 import SearchBar from "./Autofill";
+import Sort from './Sort';
 
 const Activity = () => {
   return (
     <div>
       <Filters />
+      <Sort/>
       <SearchBar />
       <List />
     </div>
--- src/components/activity/Sort.js
+++ src/components/activity/Sort.js
@@ -1,13 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { quakeFetch } from "../../actions";
 import { useForm } from "../../customHooks/useForm";
 
 function Sort(props) {
 
+    const handleChanges = (e)=>{
+        const sortBy=e.target.value;
+        console.log(sortBy);
+    }
+
     return (
         <div>
-            
+            <label for="sort">Sort by:</label>
+            <form>
+                <select name="sort" onChange={handleChanges}>
+                    <option value="newest">Newest</option>
+                    <option value="oldest">Oldest</option>
+                    <option value="ascending magnitude">Ascending Magnitude</option>
+                    <option value="descending magnitude">Descending Magnitude</option>
+                </select>
+            </form>
         </div>
     )
 }
+
+export default Sort;
