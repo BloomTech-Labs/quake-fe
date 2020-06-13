ed9574b248073add183af15ea060cf3bf88dbac6
Pete James <petejames@petes-air.lan>
2020-06-13 10:50:57 -0700

    added UserLocation component under /utils to set and get lat long

---
+++ src/utils/UserLocation.js
@@ -0,0 +1,29 @@
+const setGps = (onSuccess, onError) => {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords;
+        localStorage.setItem("gps", JSON.stringify({ latitude, longitude }));
+        console.log('setGps', latitude, longitude )
+        if (onSuccess) onSuccess(position);
+      },
+      err => {
+        console.error(err);
+        if (onError) onError(err);
+      },
+      {
+        timeout: 5000
+      }
+    );
+  };
+  
+  const getGps = () => {
+    const coords = localStorage.getItem("gps");
+    if (coords) {
+      console.log('getGPS coords', coords)
+      return coords;
+    }
+  
+    return { latitude: null, longitude: null };
+  };
+  
+  export default { setGps, getGps };
