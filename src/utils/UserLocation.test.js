d7cfe689114e33098de337050c185d7f0c281835
Pete James <petejames@petes-air.lan>
2020-06-17 19:57:21 -0700

    added tests to UserLocation and sort

---
+++ src/utils/UserLocation.test.js
@@ -0,0 +1,13 @@
+import 'jest-extended';
+import { getGps } from "./UserLocation";
+
+
+describe('User Location getGps', () => {
+    it('should return local storage', () => {
+        expect(getGps()).toBeTruthy();
+    })
+    it('should return object', () => {
+        expect(getGps()).toBeObject()
+    })
+});
+
