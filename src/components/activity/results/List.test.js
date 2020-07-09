daae16d5f021576a2c2109dcdf4defcda924f6bd
Ken Diehl <diehlkj.99@gmail.com>
2020-07-08 23:21:18 -0400

    Wrote tests for each render condition in List.js

--- src/components/activity/results/List.test.js
+++ src/components/activity/results/List.test.js
@@ -17,29 +17,158 @@ describe("List.js Tests", () => {
   let store;
   let listComponent;
 
-  beforeEach(() => {
-    store = mockStore({
-      quakeReducer: {
-        quakes: [],
-        quakeFetch: false,
-        quakeFetchError: false,
-        sortBy: "newest",
-      }
-    });
+  describe("List.js Snapshot", () => {
+    it("List.js renders with mocked state", () => {
+      store = mockStore({
+        quakeReducer: {
+          quakes: [],
+          quakeFetch: false,
+          quakeFetchError: false,
+          sortBy: "newest",
+        },
+      });
 
-    listComponent = renderer.create(
-      <Provider store={store}>
+      listComponent = renderer.create(
+        <Provider store={store}>
           <List />
-      </Provider>
-    );
-  });
-
-  it('should render with given state', () => {
-    expect(listComponent.toJSON()).toMatchSnapshot();
+        </Provider>
+      );
+      expect(listComponent.toJSON()).toMatchSnapshot();
+    });
   });
 
   // Empty quake array
+  describe("Empty Quake Array Condition", () => {
+    it("Renders no quakes message", () => {
+      store = mockStore({
+        quakeReducer: {
+          quakes: [],
+          quakeFetch: false,
+          quakeFetchError: false,
+          sortBy: "newest",
+        },
+      });
+
+      listComponent = rtl.render(
+        <Provider store={store}>
+          <List />
+        </Provider>
+      );
+
+      const element = listComponent.getByText(/Aww SHOCKS!/i);
+
+      expect(element).toBeVisible();
+    });
+  });
+
   // Maps quake array
+  describe("Card Map Condition", () => {
+    it("show no results", () => {
+      store = mockStore({
+        quakeReducer: {
+          quakes: [
+            {
+              type: "Feature",
+              properties: {
+                mag: 3.7,
+                place: "26 km SW of Goldfield, Nevada",
+                time: 1594243664887,
+                updated: 1594261197492,
+                tz: null,
+                url:
+                  "https://earthquake.usgs.gov/earthquakes/eventpage/nn00758288",
+                detail:
+                  "https://earthquake.usgs.gov/fdsnws/event/1/query?eventid=nn00758288&format=geojson",
+                felt: 4,
+                cdi: 2.7,
+                mmi: 3.33,
+                alert: null,
+                status: "reviewed",
+                tsunami: 0,
+                sig: 212,
+                net: "nn",
+                code: "00758288",
+                ids: ",nn00758288,us7000akeq,",
+                sources: ",nn,us,",
+                types: ",dyfi,moment-tensor,origin,phase-data,shakemap,",
+                nst: 28,
+                dmin: 0.269,
+                rms: 0.1421,
+                gap: 61.59,
+                magType: "ml",
+                type: "earthquake",
+                title: "M 3.7 - 26 km SW of Goldfield, Nevada",
+              },
+              geometry: {
+                type: "Point",
+                coordinates: [-117.4392, 37.5281, 9.1],
+              },
+              id: "nn00758288",
+            },
+          ],
+          quakeFetch: false,
+          quakeFetchError: false,
+          sortBy: "newest",
+        },
+      });
+
+      listComponent = rtl.render(
+        <Provider store={store}>
+          <List />
+        </Provider>
+      );
+
+      const element = listComponent.getByText(/Goldfield/i);
+
+      expect(element).toBeVisible();
+    });
+  });
+
   // Searching for quakes
-  // Error display
+  describe("Quake Search Condition", () => {
+    it("show no results", () => {
+      store = mockStore({
+        quakeReducer: {
+          quakes: [],
+          quakeFetch: true,
+          quakeFetchError: false,
+          sortBy: "newest",
+        },
+      });
+
+      listComponent = rtl.render(
+        <Provider store={store}>
+          <List />
+        </Provider>
+      );
+
+      const element = listComponent.getByText(/Searching for quakes.../i);
+
+      expect(element).toBeVisible();
+    });
+  });
+
+    // Error display
+    describe("Quake Error Condition", () => {
+      it("show no results", () => {
+        store = mockStore({
+          quakeReducer: {
+            quakes: [],
+            quakeFetch: false,
+            quakeFetchError: true,
+            sortBy: "newest",
+          },
+        });
+
+        listComponent = rtl.render(
+          <Provider store={store}>
+            <List />
+          </Provider>
+        );
+
+        const element = listComponent.getByText(/There was a problem getting your quakes.../i);
+        
+        expect(element).toBeVisible();
+      });
+    });
 });
