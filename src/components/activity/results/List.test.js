18f281a7fef868b6d8e76703f6663ae4bbc579e4
Ken Diehl <diehlkj.99@gmail.com>
2020-07-08 21:40:56 -0400

    Added react-test-renderer for snapshot testing. Added tests for searchReducer, moved reducer test file from actions folder to reducers folder. created snapshot test for list.js

--- src/components/activity/results/List.test.js
+++ src/components/activity/results/List.test.js
@@ -3,24 +3,43 @@ import { BrowserRouter as Router } from "react-router-dom";
 
 import * as rtl from "@testing-library/react";
 import "@testing-library/jest-dom/extend-expect";
+import renderer from "react-test-renderer";
 
-import { createStore, applyMiddleware } from "redux";
+import configureStore from "redux-mock-store";
 import { Provider } from "react-redux";
-import thunk from "redux-thunk";
-import { rootReducer as reducer } from "../../../reducers/index";
+// import { rootReducer as reducers } from "../../../reducers/index";
 
 import List from "./List";
 
-const store = createStore(reducer, applyMiddleware(thunk));
+const mockStore = configureStore([]);
 
-describe("<List/>", () => {
-  it("should render...", () => {
-    rtl.render(
+describe("List.js Tests", () => {
+  let store;
+  let listComponent;
+
+  beforeEach(() => {
+    store = mockStore({
+      quakeReducer: {
+        quakes: [],
+        quakeFetch: false,
+        quakeFetchError: false,
+        sortBy: "newest",
+      }
+    });
+
+    listComponent = renderer.create(
       <Provider store={store}>
-        <Router>
           <List />
-        </Router>
       </Provider>
     );
   });
+
+  it('should render with given state', () => {
+    expect(listComponent.toJSON()).toMatchSnapshot();
+  });
+
+  // Empty quake array
+  // Maps quake array
+  // Searching for quakes
+  // Error display
 });
