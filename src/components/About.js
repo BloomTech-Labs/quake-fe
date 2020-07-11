d103e2027b4c2bc48d56fd16ee5aad909ba3dc6d
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2020-07-11 01:40:56 -0700

    about-responsiveness-structure

--- src/components/About.js
+++ src/components/About.js
@@ -2,9 +2,19 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <p>WIP...About</p>
-    </div>
+    <>
+      <div className="spacer" />
+      <main className="about-main">
+        <aside>
+          <h1 className="about-h1">We are FaultLine</h1>
+          <div className="about-team-img" />
+          <div className="link-names">
+          <a className="left-link">Eddie</a><a>Laura</a><a>Ken</a><a>Jeff</a>
+          <a>Pete</a><a>Cesar</a><a>Jeremy</a><a>Nicole</a>
+          </div>
+        </aside>
+      </main>
+    </>
   );
 };
 
