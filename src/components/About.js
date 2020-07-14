f9dcdb0bd62a236e8cee3fa95cd710abd279a2b4
Eddie Jimenez <52768061+l8nightswithJS@users.noreply.github.com>
2020-07-13 18:36:28 -0700

    about us version 1 may be updated.

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,35 @@
 import React from "react";
-
+import aboutMap from "../images/about-us-map.png";
+import aboutVerticalImg from "../images/about-us-vertical.png"
 const About = () => {
   return (
     <>
       <div className="spacer" />
       <main className="about-main">
-        <aside>
-          <h1 className="about-h1">We are FaultLine</h1>
-          <div className="about-team-img" />
-          <div className="link-names">
-          <a className="left-link">Eddie</a><a>Laura</a><a>Ken</a><a>Jeff</a>
-          <a>Pete</a><a>Cesar</a><a>Jeremy</a><a>Nicole</a>
-          </div>
-        </aside>
+        <section className="about-links-container">
+          <h1>We are FaultLine</h1>
+          <aside className="about-team-img" />
+          <section className="about-team-links">
+            <a className="ej">Eddie</a>
+            <a className="ls">Laura</a>
+            <a className="kd">Ken</a>
+            <a className="jb">Jeff</a>
+            <a className="pj">Pete</a>
+            <a className="ch">Cesar</a>
+            <a className="jg">Jeremy</a>
+            <a className="nw">Nicole</a>
+          </section>
+        </section>
+
+        <section className="about-map-container">
+          <aside className="about-horizontalbg">
+            <h1>We live in 8 different states</h1>
+          </aside>
+          <aside className="about-map-verticalbg">
+            <img className="imgMap" src={aboutMap} alt="map of United States"/>
+            <img className="vertical-accent" src={aboutVerticalImg} />
+          </aside>
+        </section>
       </main>
     </>
   );
