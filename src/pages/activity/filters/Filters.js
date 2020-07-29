ad13944e9c860aac42521a462bcc5d6d337c9706
PeteDram <me@petejames.com>
2020-07-28 20:19:09 -0700

    added remaining quick filters

--- src/pages/activity/filters/Filters.js
+++ src/pages/activity/filters/Filters.js
@@ -2,6 +2,7 @@ import React, { useEffect, useRef } from "react";
 import { connect } from "react-redux";
 import { firstLoad, quakeFetch, updateSearchParams } from "../../../redux/actions";
 import useDarkMode from "../../../utils/customHooks/useDarkMode";
+
  
 function Filters({
  quakeFetch,
