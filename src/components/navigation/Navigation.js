b0236bc0e66e7ee29c3ee594d56bcfcd41477e80
cesarhj19 <cesarhj19@gmail.com>
2020-07-29 18:35:11 -0700

    refactored nav

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,138 +1,38 @@
 import React, { useState } from "react";
-import { NavLink} from "react-router-dom";
-import useDarkMode from "../../utils/customHooks/useDarkMode";
+import {NavLink} from "react-router-dom";
+import SubNav from './subNav'
 
 // General Icons
 import { ReactComponent as FeedIcon } from "../../images/icons/feed-icon.svg";
 import { ReactComponent as ReportIcon } from "../../images/icons/report-icon.svg";
 import { ReactComponent as ActivityIcon } from "../../images/icons/activity-icon.svg";
 import { ReactComponent as MenuIcon } from "../../images/icons/menu-icon.svg";
-import { ReactComponent as PeopleIcon } from "../../images/icons/people-icon.svg";
-import { ReactComponent as PhoneIcon } from "../../images/icons/phone-icon.svg";
-import { ReactComponent as BookIcon } from "../../images/icons/book-icon.svg";
-import { ReactComponent as ThemeIcon } from "../../images/icons/theme-icon.svg";
-import { ReactComponent as FaqIcon } from "../../images/icons/faq-icon.svg";
-import { ReactComponent as BugIcon } from "../../images/icons/bug-icon.svg";
-import { ReactComponent as FilterIcon } from "../../images/icons/filter.svg";
-import { ReactComponent as UnitIcon } from "../../images/icons/unit-toggle-icon.svg";
+
+const activeStyle = {background: '#65FFAE20'};
 
 const Navigation = () => {
   const [viewMore, setViewMore] = useState(false);
-  // Toggles Dark Mode CSS theme. See src/customHooks/useDarkMode.js
-  const [darkMode, setDarkMode] = useDarkMode("dark-mode");
-
-  const toggleDarkMode = (e) => {
-    e.preventDefault();
-    setDarkMode(!darkMode);
-  };
-
+  const toggleViewMore = () => { setViewMore(!viewMore); };
   return (
     <nav>
-      <NavLink
-        className="nav-link"
-        activeStyle={{ background: "#65FFAE20" }}
-        exact
-        to="/feed"
-      >
-        <FeedIcon className="nav-icon" />
-        <p>News</p>
+      <NavLink className="nav-link" activeStyle={activeStyle} exact to="/feed">
+        <FeedIcon className="nav-icon"/> <p>News</p>
       </NavLink>
 
-      <a
-        className="nav-link"
-        target="_blank"
-        rel="noopener noreferrer"
-        href="https://earthquake.usgs.gov/earthquakes/eventpage/tellus"
-      >
-        <ReportIcon className="nav-icon" />
-        <p>Report</p>
+      <a className="nav-link" target="_blank" rel="noopener noreferrer" href="https://earthquake.usgs.gov/earthquakes/eventpage/tellus">
+        <ReportIcon className="nav-icon"/> <p>Report</p>
       </a>
 
-      <NavLink
-        className="nav-link"
-        activeStyle={{ background: "#65FFAE20" }}
-        exact
-        to="/"
-      >
-        <ActivityIcon className="nav-icon" />
-        <p>Activity</p>
+      <NavLink className="nav-link" activeStyle={activeStyle} exact to="/">
+        <ActivityIcon className="nav-icon"/> <p>Activity</p>
       </NavLink>
 
-      <div
-        onClick={() => {
-          setViewMore(!viewMore);
-        }}
-        className="nav-link"
-        style={
-          viewMore ? { background: "#65FFAE20" } : { background: "transparent" }
-        }
-      >
-        <MenuIcon className="nav-icon" />
-        <p>More</p>
+      <div onClick={toggleViewMore} className="nav-link" style={viewMore ? activeStyle : { background: "transparent" }}>
+        <MenuIcon className="nav-icon"/> <p>More</p>
       </div>
 
-      <div
-        onClick={() => {
-          setViewMore(!viewMore);
-        }}
-        className={viewMore ? "more-links-focus" : "more-links-hidden"}
-      >
-        <div className="more-links-container">
-          <div className="big-link-container">
-            <NavLink to="/">
-              <FeedIcon className="nav-icon" />
-              <p>Access News Feed</p>
-            </NavLink>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              href="https://earthquake.usgs.gov/earthquakes/eventpage/tellus"
-            >
-              <ReportIcon className="nav-icon" />
-              <p>Report an Event</p>
-            </a>
-
-            <NavLink to="/">
-              <FaqIcon className="nav-icon" />
-              <p>Frequently Asked Questions</p>
-            </NavLink>
-
-            <NavLink to="/report">
-              <BugIcon className="nav-icon" />
-              <p>Report a Bug</p>
-            </NavLink>
-
-            <NavLink to="/" onClick={toggleDarkMode}>
-              <ThemeIcon className="nav-icon" />
-              <p>Toggle Light/Dark Theme</p>
-            </NavLink>
-
-            <NavLink to="/" >
-              <UnitIcon className="nav-icon" />
-              <p>Toggle Kilometers/Miles</p>
-            </NavLink>
-
-            <NavLink to="/about">
-              <PeopleIcon className="nav-icon" />
-              <p>About Us</p>
-            </NavLink>
-
-            <NavLink to="/">
-              <PhoneIcon className="nav-icon" />
-              <p>Setup Notifications</p>
-            </NavLink>
-
-            <NavLink to="/resources">
-              <BookIcon className="nav-icon" />
-              <p>Survival Info</p>
-            </NavLink>
-
-            <NavLink to="/">
-              <FilterIcon className="nav-icon" />
-              <p>Filter Results</p>
-            </NavLink>
-          </div>
-        </div>
+      <div onClick={toggleViewMore} className={viewMore ? "more-links-focus" : "more-links-hidden"}>
+        <SubNav />
       </div>
     </nav>
   );
