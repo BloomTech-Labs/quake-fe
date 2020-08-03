6e34111a48d14ac692da1151ec15736ef37c30b7
Jeff Breig <jjbreig@gmail.com>
2020-08-02 21:05:26 -0400

    adding card file

---
+++ src/pages/feed/Card.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = () => {
+  return (
+    <div className="main-container no-scroll">
+      <p>WIP...Card</p>
+    </div>
+  );
+};
+
+export default Card;
