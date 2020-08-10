5b344cd6b48d58ab494f2a6b06d968a335e79302
Jeff Breig <jjbreig@gmail.com>
2020-08-09 22:29:48 -0400

    fixed image rendering

--- src/pages/feed/Card.js
+++ src/pages/feed/Card.js
@@ -16,7 +16,7 @@ const Card = (newsArticle) => {
           <h2 className="news-article-title">{newsArticle.newsArticle.headline.main}</h2>
           <div className="news-article-bottom-info">
             <h3 className="news-article-topic">{newsArticle.newsArticle.keywords[0].value}</h3>
-            <time className="news-article-date">{newsArticle.newsArticle.publishedAt}</time>
+            <time className="news-article-date">{newsArticle.newsArticle.pub_date}</time>
           </div>
         </div>
         <img src={articleImage} className="news-article-image"/>
