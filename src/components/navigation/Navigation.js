b9cbe1d734425a16111af25618026f4ef23276b9
Ken Diehl <diehlkj.99@gmail.com>
2020-08-12 23:07:23 -0400

    Completed activity component styling and scaling for desktop and tablet

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import {NavLink} from "react-router-dom";
-import SubNav from './subNav'
+import { NavLink } from "react-router-dom";
+import SubNav from "./subNav";
 
 // General Icons
 import { ReactComponent as FeedIcon } from "../../images/icons/feed-icon.svg";
@@ -8,30 +8,44 @@ import { ReactComponent as ReportIcon } from "../../images/icons/report-icon.svg
 import { ReactComponent as ActivityIcon } from "../../images/icons/activity-icon.svg";
 import { ReactComponent as MenuIcon } from "../../images/icons/menu-icon.svg";
 
-const activeStyle = {background: '#65FFAE20'};
+const activeStyle = { background: "#65FFAE20" };
 
 const Navigation = () => {
   const [viewMore, setViewMore] = useState(false);
-  const toggleViewMore = () => { setViewMore(!viewMore); };
+  const toggleViewMore = () => {
+    setViewMore(!viewMore);
+  };
   return (
     <nav>
       <NavLink className="nav-link" activeStyle={activeStyle} exact to="/feed">
-        <FeedIcon className="nav-icon"/> <p>News</p>
+        <FeedIcon className="nav-icon" /> <p>News</p>
       </NavLink>
 
-      <a className="nav-link" target="_blank" rel="noopener noreferrer" href="https://earthquake.usgs.gov/earthquakes/eventpage/tellus">
-        <ReportIcon className="nav-icon"/> <p>Report</p>
+      <a
+        className="nav-link"
+        target="_blank"
+        rel="noopener noreferrer"
+        href="https://earthquake.usgs.gov/earthquakes/eventpage/tellus"
+      >
+        <ReportIcon className="nav-icon" /> <p>Report</p>
       </a>
 
       <NavLink className="nav-link" activeStyle={activeStyle} exact to="/">
-        <ActivityIcon className="nav-icon"/> <p>Activity</p>
+        <ActivityIcon className="nav-icon" /> <p>Activity</p>
       </NavLink>
 
-      <div onClick={toggleViewMore} className="nav-link" style={viewMore ? activeStyle : { background: "transparent" }}>
-        <MenuIcon className="nav-icon"/> <p>More</p>
+      <div
+        onClick={toggleViewMore}
+        className="nav-link"
+        style={viewMore ? activeStyle : { background: "transparent" }}
+      >
+        <MenuIcon className="nav-icon" /> <p>More</p>
       </div>
 
-      <div onClick={toggleViewMore} className={viewMore ? "more-links-focus" : "more-links-hidden"}>
+      <div
+        onClick={toggleViewMore}
+        className={viewMore ? "more-links-focus" : "more-links-hidden"}
+      >
         <SubNav />
       </div>
     </nav>
--- src/pages/activity/results/ResultsContainer.js
+++ src/pages/activity/results/ResultsContainer.js
@@ -3,22 +3,42 @@ import React, { useState, useEffect } from "react";
 import MediaQuery, { useMediaQuery } from "react-responsive";
 import Sort from "./Sort";
 import List from "./List";
-import ViewType from './viewType'
+import ViewType from "./viewType";
 
 const view = [
-  {view: 'map', cheight: 'inherit', ctransform: 'translateY(calc(100% - 30px))', mheight: 'calc(100% - 30px)'},
-  {view: 'combo', cheight: 'calc(65% - 45px)', ctransform: 'translateY(0)', mheight: 'calc(35% + 45px)'},
-  {view: 'list', cheight: 'calc(100% - 45px)', ctransform: 'translateY(0)', mheight: '45px'},
-  {view: 'hcombo', cheight: '100%', ctransform: 'translateY(0)', mheight: '100%'},
+  {
+    view: "map",
+    cheight: "inherit",
+    ctransform: "translateY(calc(100% - 30px))",
+    mheight: "calc(100% - 30px)",
+  },
+  {
+    view: "combo",
+    cheight: "calc(65% - 45px)",
+    ctransform: "translateY(0)",
+    mheight: "calc(35% + 45px)",
+  },
+  {
+    view: "list",
+    cheight: "calc(100% - 45px)",
+    ctransform: "translateY(0)",
+    mheight: "45px",
+  },
+  {
+    view: "hcombo",
+    cheight: "100%",
+    ctransform: "translateY(0)",
+    mheight: "100%",
+  },
 ];
 
 const ResultsContainer = () => {
   const [viewType, setViewType] = useState("hcombo");
-  
+
   const isBigScreen = useMediaQuery({
-    query: '(min-width: 650px)'
+    query: "(min-width: 650px)",
   });
-  
+
   // if (isBigScreen) {
   //   setViewType("hcombo");
   // }
@@ -27,8 +47,8 @@ const ResultsContainer = () => {
     const container = document.getElementById("results-container");
     const map = document.getElementById("map-container");
 
-    for (let i=0; i<view.length; i++) {
-      if(viewType === view[i].view) {
+    for (let i = 0; i < view.length; i++) {
+      if (viewType === view[i].view) {
         container.style.height = view[i].cheight;
         container.style.transform = view[i].ctransform;
         map.style.height = view[i].mheight;
@@ -39,7 +59,7 @@ const ResultsContainer = () => {
   return (
     <div id="results-container" className="results-container no-scroll">
       <MediaQuery maxWidth={650}>
-        <ViewType viewType={viewType} setViewType={setViewType}/>
+        <ViewType viewType={viewType} setViewType={setViewType} />
       </MediaQuery>
       <Sort />
       <div className="earthquake-list-container scroll">
