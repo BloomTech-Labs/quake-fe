3324c6e7c962305722698293bf623d160a1aebff
PeteDram <me@petejames.com>
2020-08-29 15:37:14 -0700

    added 192 and 512px icons to manifest

--- src/components/navigation/subNav.js
+++ src/components/navigation/subNav.js
@@ -23,11 +23,34 @@ const SubNav = () => {
   // Toggles Dark Mode CSS theme. See src/customHooks/useDarkMode.js
   const [darkMode, setDarkMode] = useDarkMode("dark-mode");
   const toggleDarkMode = (e) => {e.preventDefault();setDarkMode(!darkMode);};
+  
+  // Detects if device is on iOS 
+  const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+  }
+  // Detects if device is in standalone mode
+  const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+  // Checks if should display install popup notification:
+  const triggerHomescreen = () => {
+  //   if (isIos() && !isInStandaloneMode()) {
+  //     // this.setState({ showInstallMessage: true });
+  //     alert('iOS! and not in Standalone mode')
+  //   } else if (window.matchMedia('(display-mode: standalone)')) {
+  //     alert('in standalone mode')
+  //     alert(window.matchMedia)
+  //   } else if (window.matchMedia(!'(display-mode: standalone)')) {
+  //   alert('not in standalone mode')
+  // }
+
+  }
+  
 
   const linksList = [
     {to: '/report', Icon: BugIcon, text: 'Report a Bug', onClick: null},
     {to: '/', Icon: ThemeIcon, text: 'Toggle Light/Dark Theme', onClick: toggleDarkMode},
-    {to: '/', Icon: UnitIcon, text: 'Toggle Kilometers/Miles', onClick: null},
+    {to: '/', Icon: UnitIcon, text: 'Toggle Kilometers/Miles', className: 'add-button', onClick: triggerHomescreen},
     {to: '/about', Icon: PeopleIcon, text: 'About Us', onClick: null},
     {to: '/sms', Icon: PhoneIcon, text: 'Setup Notifications', onClick: null},
     {to: '/resources', Icon: BookIcon, text: 'Survival Info', onClick: null},
@@ -37,7 +60,7 @@ const SubNav = () => {
     <div className="more-links-container">
       <div className="big-link-container">
         {linksList.map((link, index) => {
-          return <Links to={link.to} Icon={link.Icon} text={link.text} click={link.onClick} key={index}/>
+          return <Links to={link.to} Icon={link.Icon} text={link.text} click={link.onClick} key={index} className={link.className} />
         })}
       </div>
     </div>
